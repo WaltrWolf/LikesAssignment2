ca37c99737d4d8c79944662cad8c6fea56a10e34
WaltrWolf <guswolf@wolfinco.com>
2022-02-02 08:59:18 -0500

    done

---
+++ script.js
@@ -0,0 +1,9 @@
+function like(){
+    document.querySelector(".likeAmount").innerHTML++;
+}
+function like2(){
+    document.querySelector(".likeAmount2").innerHTML++;
+}
+function like3(){
+    document.querySelector(".likeAmount3").innerHTML++;
+}
